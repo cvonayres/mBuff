f9de3d2c3a0bc0f2e9b63ad8b5e2a1401b1b55ec
ecm <carl.ayres@live.co.uk>
2025-08-25 21:20:50 +0100

    backend working, combined docker compose files.

--- ws/src/msense_hat_node/msense_hat_node/msense_hat.py
+++ ws/src/msense_hat_node/msense_hat_node/msense_hat.py
@@ -1,14 +1,17 @@
+import os, sys
 import rclpy
 from rclpy.node import Node
 
 class MSenseHatNode(Node):
     def __init__(self):
         super().__init__('msense_hat')
-        self.timer = self.create_timer(2.0, self._tick_callback)
+        debug = ('-d' in sys.argv) or ('--debug' in sys.argv) or (os.getenv('DEBUG') == '1')
+        if debug:
+            self._timer = self.create_timer(2.0, self._tick_callback)
         self.get_logger().info('msense_hat node up (stub)')
 
     def _tick_callback(self):
-        self.get_logger().info('msense_hat heartbeat…')
+        self.get_logger().debug('msense_hat heartbeat…')
 
 def main():
     rclpy.init()
