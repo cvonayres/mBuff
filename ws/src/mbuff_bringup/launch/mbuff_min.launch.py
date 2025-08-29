8fede8288f980ab520c0cb77d776ea9dfc9c1ce9
ECM <101433020+cvonayres@users.noreply.github.com>
2025-08-29 07:57:19 -0700

    Update mbuff_min.launch.py to include mbot_controller_node

--- ws/src/mbuff_bringup/launch/mbuff_min.launch.py
+++ ws/src/mbuff_bringup/launch/mbuff_min.launch.py
@@ -1,8 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
         Node(package='msense_hat_node', executable='msense_hat', name='msense_hat'),
         Node(package='web_bridge_node', executable='web_bridge', name='web_bridge'),
+        Node(package='mbot_controller_node', executable='mbot_controller', name='mbot_controller'),
     ])
